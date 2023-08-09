66c781fd32be895d76e96b42c059841f72acf02b
High23 <warenasb2@gmail.com>
2023-08-08 23:18:41 -0700

    Added project constructor and functionality

--- src/app.js
+++ src/app.js
@@ -1,27 +1,38 @@
-export default addTodosToProject;
+export {addTodosToProject, removeTodosFromProject, removeProject};
+
 
 const ToDo = (title, dueDate, priority, notes, checkList) => {
-    const item = {
+    const items = {
         title,
         dueDate,
         priority,
         notes,
         checkList
     }
-    return item
+    return items
 }
 
+const Project = () => { 
+    return {list : []}
+}
+
+let project = Project();
 
 function addTodosToProject() {
-    
+    const todo = ToDo("Title", "due date", "priority", "notes", false);
+    const todo2 = ToDo("Title", "due date", "priority", "notes", false);
+    project.list.push(todo)
+    project.list.push(todo2)
+    console.table(project.list);
 }
 
-
 function removeTodosFromProject() {
-    
+    project.list.splice(0, 1);
+    console.table(project.list);
 }
 
 function removeProject() {
-
+    project = [];
+    console.table(project);
 }
 
