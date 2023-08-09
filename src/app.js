ec479dc731a8736a503300c7e9d4d7b6e29da3fe
High23 <warenasb2@gmail.com>
2023-08-08 21:57:39 -0700

    Added todo constructor

---
+++ src/app.js
@@ -0,0 +1,27 @@
+export default addTodosToProject;
+
+const ToDo = (title, dueDate, priority, notes, checkList) => {
+    const item = {
+        title,
+        dueDate,
+        priority,
+        notes,
+        checkList
+    }
+    return item
+}
+
+
+function addTodosToProject() {
+    
+}
+
+
+function removeTodosFromProject() {
+    
+}
+
+function removeProject() {
+
+}
+
