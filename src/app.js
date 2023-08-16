c225954d57dc8c65ad751311f0d820c1ca8c01b8
High23 <warenasb2@gmail.com>
2023-08-15 22:38:55 -0700

    Now only creates a todo object. Will rename later

--- src/app.js
+++ src/app.js
@@ -1,38 +1,13 @@
-export {addTodosToProject, removeTodosFromProject, removeProject};
+export {CreateToDo};
 
 
-const ToDo = (title, dueDate, priority, notes, checkList) => {
-    const items = {
-        title,
-        dueDate,
-        priority,
-        notes,
-        checkList
-    }
-    return items
-}
-
-const Project = () => { 
-    return {list : []}
-}
-
-let project = Project();
-
-function addTodosToProject() {
-    const todo = ToDo("Title", "due date", "priority", "notes", false);
-    const todo2 = ToDo("Title", "due date", "priority", "notes", false);
-    project.list.push(todo)
-    project.list.push(todo2)
-    console.table(project.list);
-}
-
-function removeTodosFromProject() {
-    project.list.splice(0, 1);
-    console.table(project.list);
-}
-
-function removeProject() {
-    project = [];
-    console.table(project);
+function CreateToDo(submittedForm, getTaskID) {
+    const id = getTaskID;
+    const titleName = submittedForm[0].childNodes[3].value;
+    const dueDate = submittedForm[1].childNodes[3].value;
+    const priority = submittedForm[2].childNodes[3].value;
+    const note = submittedForm[3].childNodes[3].value;
+    const todo = {id, titleName, dueDate, priority, note}
+    return todo;
 }
 
