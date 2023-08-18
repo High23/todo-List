8b25debcfecdd105e04b8e1fb4b91ccc39ea1c82
High23 <warenasb2@gmail.com>
2023-08-17 17:27:06 -0700

    The today, week, and month tabs now work
    The user can now click on the sidebar tabs. Doing so will show all the
    ToDos that align with the tab's time-frame (ie. today, week, month).

--- src/tabs.js
+++ src/tabs.js
@@ -1,13 +1,124 @@
-import { clearTab } from "./UI";
+import { createTask } from "./ToDo";
+import { clearTab, deleteAddTaskDiv } from "./UI";
 import { loadTodos } from "./storage";
+import {format, isToday, isThisWeek, isThisMonth, startOfWeek, endOfWeek} from "date-fns";
 
-export {inboxTab}
+export {inboxTab, todayTab, weekTab, monthTab}
 
 function inboxTab() {
-    const inboxTab = document.querySelector('.sidebar-tab, .inbox');
+    const inboxTab = document.querySelector('.inbox');
     inboxTab.addEventListener('click', () => {
+        const tabTitle = document.querySelector('.title-of-tab');
+        tabTitle.innerHTML = 'Inbox'
         clearTab();
         const inbox = JSON.parse(localStorage.getItem('inbox'));
         loadTodos(inbox);
     });
+}
+
+function todayTab() {
+    const todayTab = document.querySelector('.today');
+    todayTab.addEventListener('click', () => {
+        let tabName = 'Today';
+        clearTab();
+        deleteAddTaskDiv();
+        getTabToDos(tabName);
+        adjustTabInfo(tabName)
+    })
+}
+
+function weekTab() {
+    const weekTab = document.querySelector('.week');
+    weekTab.addEventListener('click', () => {
+        let tabName = 'Week'
+        clearTab();
+        deleteAddTaskDiv();
+        getTabToDos(tabName)
+        adjustTabInfo(tabName)
+    })
+}
+ 
+function monthTab() {
+    const monthTab = document.querySelector('.month');
+    monthTab.addEventListener('click', () => {
+        let tabName = 'Month';
+        clearTab();
+        deleteAddTaskDiv();
+        getTabToDos(tabName)
+        adjustTabInfo(tabName)
+    })
+}
+
+
+function getTabToDos(tabName) {
+    const inbox = JSON.parse(localStorage.getItem('inbox'));
+    const projects = JSON.parse(localStorage.getItem('projects'))
+    for (let i = 0; i < inbox.length; i++) {
+        if (isToday(new Date(inbox[i].dueDate)) && tabName === 'Today') {
+            createTask(inbox[i].titleName, 
+                inbox[i].dueDate, 
+                inbox[i].priority, 
+                inbox[i].note, 
+                inbox[i].id)
+        } else if (isThisWeek(new Date(inbox[i].dueDate)) && tabName === 'Week') {
+            createTask(inbox[i].titleName, 
+                inbox[i].dueDate, 
+                inbox[i].priority, 
+                inbox[i].note, 
+                inbox[i].id)
+        } else if (isThisMonth(new Date(inbox[i].dueDate)) && tabName === ' Month') {
+            createTask(inbox[i].titleName, 
+                inbox[i].dueDate, 
+                inbox[i].priority, 
+                inbox[i].note, 
+                inbox[i].id)
+        }
+    }
+
+    for (let i = 0; i < projects.length; i++) {
+        let projectName = Object.getOwnPropertyNames(projects[i])[0];
+        let project = projects[i][[projectName]]
+        project.forEach(todo => {
+            if (isToday(new Date(todo.dueDate)) && tabName === 'Today') {
+                createTask(todo.titleName, 
+                            todo.dueDate, 
+                            todo.priority, 
+                            todo.note, 
+                            todo.id)
+            } else if (isThisWeek(new Date(todo.dueDate)) && tabName === 'Week') {
+                createTask(todo.titleName,
+                            todo.dueDate, 
+                            todo.priority, 
+                            todo.note, 
+                            todo.id)
+            } else if (isThisMonth(new Date(todo.dueDate)) && tabName === 'Month') {
+                createTask(todo.titleName,
+                            todo.dueDate, 
+                            todo.priority, 
+                            todo.note, 
+                            todo.id)
+            }
+        });
+    }
+}
+
+function adjustTabInfo(tabName) {
+    // To understand the format function, visit this website for documentation: https://date-fns.org/v2.30.0/docs/format
+    const tabTitle = document.querySelector('.title-of-tab');
+    const span = document.createElement('span');
+    if (tabName === 'Today') {
+        const dateSpan = document.querySelectorAll('.date');
+        dateSpan.forEach(span => {
+            span.remove()
+        });
+        tabTitle.textContent = tabName
+        span.innerHTML = `${format(new Date(), 'MMM do')}` 
+        tabTitle.appendChild(span);
+    } else if (tabName === 'Week') {
+        tabTitle.textContent = `This ${tabName}`
+        span.innerHTML = `${format(startOfWeek(new Date()), 'MM/dd')} - ${format(endOfWeek(new Date()), 'MM/dd')}`
+        tabTitle.appendChild(span);
+    } else if (tabName === 'Month') {
+        tabTitle.innerHTML = `${format(new Date(), 'MMMM')}`
+    }    
 }
