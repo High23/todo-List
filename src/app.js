65e7a30c577e004b2d281857d0d4d5a6f9521cdf
High23 <warenasb2@gmail.com>
2023-08-17 23:52:03 -0700

    Changed formatting of due date

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
+import { format } from "date-fns";
+
 export {CreateToDo};
 
 
 function CreateToDo(submittedForm, getTaskID) {
     const id = getTaskID;
     const titleName = submittedForm[0].childNodes[3].value;
-    const dueDate = submittedForm[1].childNodes[3].value;
+    let dueDate = submittedForm[1].childNodes[3].value;
+    dueDate = dueDate.split('-')
+    dueDate = format(new Date(dueDate[0], dueDate[1] - 1, dueDate[2]), 'MM/dd/yyyy')
     const priority = submittedForm[2].childNodes[3].value;
     const note = submittedForm[3].childNodes[3].value;
-    const todo = {id, titleName, dueDate, priority, note}
-    return todo;
+    return {id, titleName, dueDate, priority, note};
 }
 
