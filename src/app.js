ae47667eee96b14fe5b50d4895a447f162d8401c
High23 <warenasb2@gmail.com>
2023-08-18 20:49:58 -0700

    Added function to update project and tasks id

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 import { format } from "date-fns";
 
-export {CreateToDo};
+export {CreateToDo, updateID};
 
 
-function CreateToDo(submittedForm, getTaskID) {
-    const id = getTaskID;
+function CreateToDo(submittedForm) {
     const titleName = submittedForm[0].childNodes[3].value;
     let dueDate = submittedForm[1].childNodes[3].value;
     dueDate = dueDate.split('-')
     dueDate = format(new Date(dueDate[0], dueDate[1] - 1, dueDate[2]), 'MM/dd/yyyy')
     const priority = submittedForm[2].childNodes[3].value;
     const note = submittedForm[3].childNodes[3].value;
-    return {id, titleName, dueDate, priority, note};
+    return {titleName, dueDate, priority, note};
+}
+
+function updateID(element, type='') {
+    if (type === 'task') {
+        const tasks = document.querySelectorAll('.task')
+        tasks.forEach((task, index) =>{
+            task.dataset.id = index;
+        })
+        return;
+    }
+    return Number(element.dataset.id);
 }
 
